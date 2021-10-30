80634ed968b7b9d95fff5d7449e36cf70db1cc74
Nowroz Shahriar <nowroz.shahriar@gmail.com>
2021-10-31 04:17:22 +0600

    1.2

--- index.js
+++ index.js
@@ -1,18 +1,66 @@
 const express = require('express');
-const { MongoClient } = require('mongodb');
+const { MongoClient, Collection } = require('mongodb');
 require('dotenv').config();
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.skciu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+async function run() {
+    try{
+        await client.connect();
+        const database = client.db('good-travel');
+        const servicesCollection = database.collection('services');
+        const ordersCollection = database.collection('orders')
+
+        // GET Services API
+        app.get('/services', async (req, res) => {
+            const cursor = servicesCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+        // GET Orders API
+        app.get('/orders', async (req, res) => {
+            const cursor = ordersCollection.find({});
+            const orders = await cursor.toArray();
+            res.send(orders);
+        })
+
+        // POST Orders API
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await ordersCollection.insertOne(order);
+            res.send(result);
+        })
+
+        
+
+
+
+
+
+
+
+
+
+
+    } finally {
+
+    }
+}
+
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
     res.send('Running Good Travel')
 })
 
 app.listen(port, () => {
-    console.log('port:', port);
+    console.log('listening to port:', port);
 })
